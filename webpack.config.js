bf05403aa637462e51f96325f1e48da7c4929724
Vaughn Haynes <drbrounsuga@gmail.com>
2016-11-30 20:55:08 -0600

    chapter 2 - webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  entry: './src/main.js',
+  //entry: './src/main.js',
+  entry: './main.coffee',
   output: {
     path: 'build',
     filename: 'bundle.js'
+  },
+  module: {
+    /*loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]*/
+    loaders: [
+      {
+        test: /\.coffee$/,
+        exclude: /(node_modules)/,
+        loader: 'coffee'
+      }
+    ]
   }
 };
